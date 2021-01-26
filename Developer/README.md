9fa491fec307f38b2c6032479f1bc05fd9d6da51
prabhakaran <prabhakaran.webandcrafts@gmail.com>
2021-01-26 18:44:09 +0530

    removed un used files

--- Developer/README.md
+++
@@ -1,1 +0,0 @@
-The Wac_Developer module provides functionality to make it easier to develop in Magento 2.
--- Developer/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use \Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'Wac_Developer',
-    __DIR__
-);
